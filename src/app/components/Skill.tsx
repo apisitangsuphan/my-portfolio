606506a5919f2ba78b380a4e4b320f73cd5c63d7
Apisit Angsuphan <60085220+sussus003@users.noreply.github.com>
2025-02-12 14:37:49 +0700

    reset to old branch

--- src/app/components/Skill.tsx
+++ src/app/components/Skill.tsx
@@ -6,13 +6,11 @@ function Skill() {
       <div className="container w-screen">
         <h3 className="text-3xl py-1 font-medium">My Skills :</h3>
         <p className="text-xl mt-7 px-20 text-gray-700">
-          I specialize in creating custom, high-quality websites and
-          applications using modern technologies.{" "}
+          
         </p>
         <p className="text-xl mt-7 px-20 text-gray-700" >
          
-          I offer a wide range of services, including website development,
-          mobile app development, and digital marketing.
+          
         </p>
       </div>
     </section>
